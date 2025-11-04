08b98388e139281288c24a39e8ef4560395fef7b
Patrícia <patricia.sl.2587@gmail.com>
2025-11-04 17:35:12 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def in_autotests_we_trust(a, b):
+    if a == b:
+        print('Passou no teste')
+    else:
+        print('Falhou no teste')
+        in_autotests_we_trust(10, '10')
+        in_autotests_we_trust(0, False)
